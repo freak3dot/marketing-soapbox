8b8116c8f99e55208d9fc40f6c8b17e7b938205d
Ryan Johnston <github@shopandlearn.net>
2015-04-16 22:39:52 -0400

    Update the min length requirment ment on name to 3

--- app/models/action.js
+++ app/models/action.js
@@ -20,7 +20,7 @@ module.exports = function (orm, db) {
 		},
 		'validations': {
 				'name': [
-					orm.enforce.ranges.length(6, undefined, "must be at least 6 chars long"),
+					orm.enforce.ranges.length(3, undefined, "must be at least 3 chars long"),
 					orm.enforce.ranges.length(undefined, 96, "cannot be longer than 96 letters")
 				],
 		},
